9664c528d838a32eb617a0fb2f90281a234aa6c4
John Helveston <john.helveston@gmail.com>
2021-07-09 21:41:28 -0400

    Update 2compare_us_data.R

--- code/2compare_us_data.R
+++ code/2compare_us_data.R
@@ -68,29 +68,33 @@ nrelSeia %>%
 # Based on these comparisons, we use SEIA data for installed capacity as
 # it tracks with NREL and covers a longer time period
 
-# Compare NREL and LBNL Cost data ---------------------------------------------
+# Compare NREL, LBNL, and SPV Cost data ---------------------------------------------
 
-nrel_lbnl_compare <- data$nrelCost %>%
+cost_compare <- data$nrelCost %>%
   mutate(source = "NREL") %>%
   rbind(
     data$lbnlCost %>%
       filter(component == "Module") %>% 
       mutate(source = "LBNL") %>% 
-      select(-component, -componentType))
+      select(-component, -componentType)) %>% 
+  filter(installType == "Utility") %>% 
+  select(-installType) %>% 
+  rbind(
+    data$usSpvCost %>% 
+      mutate(source = "SPV"))
 
-nrel_lbnl_compare %>%
+cost_compare %>%
   ggplot(aes(x = year, y = costPerKw, color = source, group = source)) +
-  geom_line() +
+  geom_line(alpha = 0.5) +
   geom_point(pch = 21, fill = "white") +
-  facet_wrap(vars(installType)) +
   scale_color_manual(
-    values = c("red", "dodgerblue"),
-    breaks = c("NREL", "LBNL")) +
+    values = c("red", "dodgerblue", "black"),
+    breaks = c("NREL", "LBNL", "SPV")) +
   theme_bw() +
   labs(x = NULL,
        y = "Cost per Kw ($USD)",
        color = "Data source",
-       title = "NREL vs. LBNL: Cost Comparison")
+       title = "Cost Comparison")
 
 # NREL and LBNL cost data are relatively similar for modules, with 
 # the biggest disagreement in the earlier years. We decided to use LBNL cost
