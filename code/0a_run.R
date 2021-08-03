d4a466aa28f7f7c25f2773b21f10809a12dd9603
John Helveston <john.helveston@gmail.com>
2021-08-03 05:01:34 -0400

    updated file names to start sensitivity checks

--- code/0a_run.R
+++ code/0a_run.R
@@ -7,16 +7,19 @@ source(here::here('code', '0setup.R'))
 source(here::here('code', '1format_data.R'))
 
 # Estimate learning models
-source(here::here('code', '3learning_curves.R'))
+source(here::here('code', '2learning_curves.R'))
 
 # Compute historical cost scenarios
-source(here::here('code', '4historical_scenarios.R'))
+source(here::here('code', '3historical_scenarios.R'))
  
 # Compute future cost scenarios
-source(here::here('code', '5projection_scenarios.R'))
+source(here::here('code', '4projection_scenarios.R'))
 
 # Make all charts
-source(here::here('code', '6charts.R'))
+source(here::here('code', '5charts.R'))
 
 # Print summary of all results
-source(here::here('code', '7summary.R'))
+source(here::here('code', '6summary.R'))
+
+# Run sensitivity checks
+source(here::here('code', '7sensitivity.R'))
