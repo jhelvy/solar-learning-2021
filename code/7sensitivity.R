a30a3e671d4156c5bb01d72fef47276ad95a186a
John Helveston <john.helveston@gmail.com>
2021-08-04 14:20:37 -0400

    added charts comparison data sources

--- code/7sensitivity.R
+++ code/7sensitivity.R
@@ -19,9 +19,9 @@ nrelSeia <- data$nrelCapacity %>%
     source = fct_relevel(source, c("NREL", "SEIA")))
 
 # Plot NREL vs. SEIA capacity comparison 
-nrelSeia %>% 
+sens_compare_capacity_type <- nrelSeia %>% 
   ggplot(aes(x = year, y = cumCapacityGw, color = source)) +
-  geom_line() +
+  geom_line(alpha = 0.5) +
   geom_point(pch = 21, fill = "white") +
   scale_color_manual(
     values = c("red", "dodgerblue"),
@@ -32,14 +32,19 @@ nrelSeia %>%
     x = NULL,
     y = "Installed Capacity (GW)",
     color = "Data source",
-    title = "Cumulative Installed Capacity (GW)")
+    title = "Comparison of Installed Capacity by Type and Data Source")
+
+ggsave(here::here(dir$figs, 'pdf', "sens_compare_capacity_type.pdf"),
+       sens_compare_capacity_type, width = 9, height = 3, device = cairo_pdf)
+ggsave(here::here(dir$figs, 'png', "sens_compare_capacity_type.png"),
+       sens_compare_capacity_type, width = 9, height = 3, dpi = 300)
 
 # During the overlapping period, NREL and SEIA data match quite closely
 # Only deviation is that NREL Commercial installations are slightly lower
 # than those in SEIA
 
 # Merge in IRENA data to compare total capacity across all 3 sources
-nrelSeia %>% 
+sens_compare_capacity_cumulative <- nrelSeia %>% 
   group_by(year, source) %>%
   summarise(cumCapacityGw = sum(cumCapacityKw) / 10^6) %>%
   rbind(
@@ -60,7 +65,14 @@ nrelSeia %>%
     x = NULL,
     y = "Installed Capacity (GW)",
     color = "Data source",
-    title = "Cumulative Installed Capacity (GW)")
+    title = "Comparison of Cumulative Installed Capacity by Data Source")
+
+ggsave(here::here(dir$figs, 'pdf', "sens_compare_capacity_cumulative.pdf"),
+       sens_compare_capacity_cumulative, 
+       width = 5, height = 3, device = cairo_pdf)
+ggsave(here::here(dir$figs, 'png', "sens_compare_capacity_cumulative.png"),
+       sens_compare_capacity_cumulative, 
+       width = 5, height = 3, dpi = 300)
 
 # IRENA data track differently from NREL and SEIA. 
 # They're slightly higher in the period before 2014 and lower afterwards
@@ -68,7 +80,7 @@ nrelSeia %>%
 # Based on these comparisons, we use SEIA data for installed capacity as
 # it tracks with NREL and covers a longer time period
 
-# Compare NREL, LBNL, and SPV Cost data ---------------------------------------------
+# Compare NREL, LBNL, and SPV Cost data --------------------------------------
 
 cost_compare <- data$nrelCost %>%
   filter(installType == "Utility") %>% 
@@ -82,7 +94,7 @@ cost_compare <- data$nrelCost %>%
       select(year, costPerKw, source)
   ) 
 
-cost_compare %>%
+sens_compare_cost <- cost_compare %>%
   ggplot(aes(x = year, y = costPerKw, color = source, group = source)) +
   geom_line(alpha = 0.5) +
   geom_point(pch = 21, fill = "white") +
@@ -91,11 +103,20 @@ cost_compare %>%
     breaks = c("NREL", "LBNL", "SPV")) +
   theme_bw() +
   labs(x = NULL,
-       y = "Cost per Kw ($USD)",
+       y = "Cost per kW ($USD)",
        color = "Data source",
-       title = "Cost Comparison")
+       title = "Comparison of Cost per kW by Data Source")
+
+ggsave(here::here(dir$figs, 'pdf', "sens_compare_cost.pdf"),
+       sens_compare_cost, width = 5, height = 3, device = cairo_pdf)
+ggsave(here::here(dir$figs, 'png', "sens_compare_cost.png"),
+       sens_compare_cost, width = 5, height = 3, dpi = 300)
 
 # NREL and LBNL cost data are relatively similar for modules, with 
 # the biggest disagreement in the earlier years. We decided to use LBNL cost
 # data for the earlier period (2000 - 2018) and NREL for 2019 & 2020 as 
 # the two appear to converge in the later years.
+
+# Compare predicted 2030 costs based on different assumptions ----------------
+
+
