98b12dd98cad6c0106ccd84e06ed5fc5b065266d
John Paul Helveston <john.helveston@gmail.com>
2022-02-10 08:33:07 -0500

    some revisions to the app

--- app.R
+++ app.R
@@ -1,21 +1,30 @@
 
 # load custom functions
-run_model <- function(df) {
+run_model <- function(df, lambda) {
+    # Run the linear model for a given lambda
+    temp <- df %>%
+        mutate(
+            log_q = log(cumCapKw_world - (lambda*cumCapKw_other)),
+            log_c = log(costPerKw),
+            log_p = log(price_si)
+        )
+    model <- lm(formula = log_c ~ log_q + log_p, data = temp)
+    err <- sum(model$residuals^2)
+    return(list(model = model, err = err))
+}
+
+run_models <- function(df) {
+    
     # For every value of lambda, run the linear model
     lambdas <- seq(0, 1, by = 0.005)
     models <- list()
     errs <- list()
     for (i in 1:length(lambdas)) {
-        temp <- df %>%
-            mutate(
-                log_q = log(cumCapKw_world - (lambdas[i]*cumCapKw_other)),
-                log_c = log(costPerKw),
-                log_p = log(price_si)
-            )
-        model <- lm(formula = log_c ~ log_q + log_p, data = temp)
-        models[[i]] <- model
-        errs[[i]] <- sum(model$residuals^2)
+        result <- run_model(df, lambdas[i])
+        models[[i]] <- result$model
+        errs[[i]] <- result$err
     }
+    
     # Get the best model based on the lowest error
     err <- unlist(errs)
     # plot(lambdas, err)
@@ -47,7 +56,8 @@ predict_cost <- function(model, df, lambda) {
     y_sim <- cbind(
         y_sim,
         cumCapKw_world = df$cumCapKw_world,
-        costPerKw = df$costPerKw)
+        costPerKw = df$costPerKw, 
+        year = df$year)
     return(y_sim)
 }
 
@@ -67,7 +77,7 @@ project_cost <- function(model, df, lambda) {
     }
     y_sim <- as.data.frame(exp(y_sim))
     names(y_sim) <- c("mean", "lower", "upper")
-    y_sim <- cbind(y_sim, cumCapKw_world = df$cumCapKw_world)
+    y_sim <- cbind(y_sim, cumCapKw_world = df$cumCapKw_world, year = df$year)
     return(y_sim)
 }
 
@@ -75,41 +85,58 @@ make_historical_plot <- function(cost_national, cost_global) {
     plot <- rbind(
         cost_global %>% mutate(scenario = "global"),
         cost_national %>% mutate(scenario = "national")) %>%
+        mutate(scenario = fct_relevel(scenario, c("national", "global"))) %>% 
         ggplot() +
         geom_line(
-            mapping = aes(x = cumCapKw_world, y = mean, color = scenario),
+            mapping = aes(x = year, y = mean, color = scenario),
         ) +
         geom_ribbon(
             mapping = aes(
-                x = cumCapKw_world, ymin = lower, ymax = upper, fill = scenario),
+                x = year, ymin = lower, ymax = upper, fill = scenario),
             alpha = 0.2
         ) +
-        geom_point(aes(x = cumCapKw_world, y = costPerKw)) +
-        scale_x_log10() +
+        geom_point(aes(x = year, y = costPerKw)) +
+        scale_x_continuous(breaks = cost_global$year) +
         scale_y_log10() +
         theme_bw() +
         labs(
+            title = "Estimated Module Cost Under Global vs. National Markets",
             x = "log(Cumulative Global Installed Capacity, kW)",
             y = "log(Cost per kW)"
         )
     return(plot)
 }
 
-make_projection_plot <- function(proj_national, proj_global) {
+make_projection_plot <- function(
+    proj_national_nt, proj_global_nt, proj_national_sd, proj_global_sd
+) {
     plot <- rbind(
-        proj_global %>% mutate(scenario = "global"),
-        proj_national %>% mutate(scenario = "national")) %>%
+        proj_global_nt %>% 
+            mutate(learning = "global", scenario = "National Trends"),
+        proj_national_nt %>% 
+            mutate(learning = "national", scenario = "National Trends"),
+        proj_global_sd %>% 
+            mutate(learning = "global", scenario = "Sustainable Development"),
+        proj_national_sd %>% 
+            mutate(learning = "national", scenario = "Sustainable Development")
+        ) %>%
+        mutate(
+            learning = fct_relevel(learning, c("national", "global"))
+        ) %>% 
         ggplot() +
         geom_line(
-            mapping = aes(x = cumCapKw_world, y = mean, color = scenario),
+            mapping = aes(x = year, y = mean, color = learning),
         ) +
         geom_ribbon(
             mapping = aes(
-                x = cumCapKw_world, ymin = lower, ymax = upper, fill = scenario),
+                x = year, ymin = lower, ymax = upper, fill = learning),
             alpha = 0.2
         ) +
+        scale_x_continuous(breaks = proj_global_nt$year) +
+        facet_wrap(vars(scenario), nrow = 1) +
         theme_bw() +
         labs(
+            title = "Estimated Module Cost Under Global vs. National Markets",
             x = "log(Cumulative Global Installed Capacity, kW)",
             y = "log(Cost per kW, $USD)"
         )
@@ -139,9 +166,9 @@ df_proj_sd_list <- list(
 )
 
 # Run historical models
-results_us <- run_model(df_us)
-results_china <- run_model(df_china)
-results_germany <- run_model(df_germany)
+results_us <- run_models(df_list[["U.S."]])
+results_china <- run_models(df_list[["China"]])
+results_germany <- run_models(df_list[["Germany"]])
 
 model_list <- list(
     "U.S." = results_us$model,
@@ -154,9 +181,9 @@ lambda_list <- list(
     "Germany" = results_germany$lambda
 )
 lr_list <- list(
-    "U.S." = percent(1 - 2^coef(model_us)["log_q"]),
-    "China" = percent(1 - 2^coef(model_china)["log_q"]), 
-    "Germany" = percent(1 - 2^coef(model_germany)["log_q"])
+    "U.S." = percent(1 - 2^coef(model_list[["U.S."]])["log_q"]),
+    "China" = percent(1 - 2^coef(model_list[["China"]])["log_q"]), 
+    "Germany" = percent(1 - 2^coef(model_list[["Germany"]])["log_q"])
 )
 
 
@@ -186,25 +213,27 @@ ui <- fluidPage(
                 label = "lambda (start)",
                 min = 0,
                 max = 1,
-                value = 0.1),
+                value = 0.32),
             sliderInput(
                 inputId = "lambda_end",
                 label = "lambda (end)",
                 min = 0,
                 max = 1,
-                value = 0.9), 
-            selectInput(
-                inputId = "scenario",
-                label = "Projection scenario", 
-                choices = c("National Trends", "Sustainable Development"),
-                selected = "National Trends"
-            )
+                value = 0.9)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("historical"),
-           plotOutput("projection")
+            h2("Best Model Results:"),
+            textOutput('lr_best'),
+            textOutput('lambda_best'),
+            h2("Current Model Results:"),
+            textOutput('lr'),
+            textOutput('lambda'),
+            h2("Historical cost"),
+            plotOutput("historical"),
+            h2("Future cost"),
+            plotOutput("projection")
         )
     )
 )
@@ -213,25 +242,6 @@ ui <- fluidPage(
 server <- function(input, output) {
     
     # Get variables based on inputs
-    df <- reactive({
-        return(df_list[[input$country]])
-    })
-    
-    df_proj <- reactive({
-        if (input$scenario == "National Trends") {
-            return(df_proj_nt_list[[input$country]])
-        }
-        return(df_proj_sd_list[[input$country]])
-    })
-    
-    model <- reactive({
-        return(model_list[[input$country]])
-    })
-    
-    lr <- reactive({
-        return(lr_list[[input$country]])
-    })
-    
     lambda <- reactive({ 
         return(input$lambda_start)
     })
@@ -239,23 +249,52 @@ server <- function(input, output) {
     lambda_end <- reactive({ 
         return(input$lambda_end)
     })
-    
-    delay <- reactive({ 
-        return(input$delay)
-    })
-    
+
     lambda_nat <- reactive({
         temp <- seq(input$lambda_start, input$lambda_end, length.out = delay() + 1)
         temp <- c(temp, rep(input$lambda_end, nrow(df()) - length(temp)))
         return(temp)
     })
-
+    
     lambda_nat_proj <- reactive({
         temp <- seq(input$lambda_start, input$lambda_end, length.out = delay() + 1)
-        temp <- c(temp, rep(input$lambda_end, nrow(df_proj()) - length(temp)))
+        temp <- c(temp, rep(input$lambda_end, nrow(df_proj_nt()) - length(temp)))
         return(temp)
     })
     
+    delay <- reactive({ 
+        return(input$delay)
+    })
+
+    df <- reactive({
+        return(df_list[[input$country]])
+    })
+    
+    df_proj_nt <- reactive({
+        return(df_proj_nt_list[[input$country]])
+    })
+    
+    df_proj_sd <- reactive({
+        return(df_proj_sd_list[[input$country]])
+    })
+
+    model <- reactive({
+        result <- run_model(df(), lambda())
+        return(result$model)
+    })
+
+    lr <- reactive({
+        return(percent(1 - 2^coef(model())["log_q"]))
+    })
+    
+    lr_best <- reactive({
+        return(lr_list[[input$country]])
+    })
+
+    lambda_best <- reactive({ 
+        return(lambda_list[[input$country]])
+    })
+
     # Compute outcomes
     cost_global <- reactive({ 
         return(predict_cost(model(), df(), lambda()))
@@ -265,22 +304,43 @@ server <- function(input, output) {
         return(predict_cost(model(), df(), lambda_nat()))
     })
     
-    proj_global <- reactive({ 
-        return(project_cost(model(), df_proj(), lambda())) 
+    proj_global_nt <- reactive({ 
+        return(project_cost(model(), df_proj_nt(), lambda())) 
     })
     
-    proj_national <- reactive({ 
-        return(project_cost(model(), df_proj(), lambda_nat_proj())) 
+    proj_national_nt <- reactive({ 
+        return(project_cost(model(), df_proj_nt(), lambda_nat_proj())) 
     })
+
+    proj_global_sd <- reactive({ 
+        return(project_cost(model(), df_proj_sd(), lambda())) 
+    })
+    
+    proj_national_sd <- reactive({ 
+        return(project_cost(model(), df_proj_sd(), lambda_nat_proj())) 
+    })
+    
+    # Output
+    
+    output$lr_best <- renderText(paste0("Learning Rate: ", lr_best()))
+    
+    output$lambda_best <- renderText(paste0("λ: ", lambda_best()))
+    
+    output$lr <- renderText(paste0("Learning Rate: ", lr()))
+    
+    output$lambda <- renderText(paste0("λ: ", lambda()))
     
     output$historical <- renderPlot(
         make_historical_plot(cost_national(), cost_global()),
-        width = 600, height = 400
+        width = 650, height = 400
     )
     
     output$projection <- renderPlot(
-        make_projection_plot(proj_national(), proj_global()),
-        width = 600, height = 400
+        make_projection_plot(
+            proj_national_nt(), proj_global_nt(), 
+            proj_national_sd(), proj_global_sd()
+        ),
+        width = 800, height = 350
     )
 }
 
