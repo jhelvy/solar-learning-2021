6bb3cba65390564a4c276b23b4ae10a885bf67f8
John Paul Helveston <john.helveston@gmail.com>
2022-02-17 07:49:54 -0500

    added parameter draws in lr code file

--- code/2learning_curves.R
+++ code/2learning_curves.R
@@ -14,14 +14,30 @@ lr_us <- 1 - 2^coef(model_us)["log_q"]
 lr_china <- 1 - 2^coef(model_china)["log_q"]
 lr_germany <- 1 - 2^coef(model_germany)["log_q"]
 
-# Save output 
+# Get draws of the parameters for each model
+params_us <- as.data.frame(
+    MASS::mvrnorm(10^4, coef(model_us), vcov(model_us))
+)
+params_china <- as.data.frame(
+    MASS::mvrnorm(10^4, coef(model_china), vcov(model_china))
+)
+params_germany <- as.data.frame(
+    MASS::mvrnorm(10^4, coef(model_germany), vcov(model_germany))
+)
+names(params_us) <- c("alpha", "beta", "gamma")
+names(params_china) <- c("alpha", "beta", "gamma")
+names(params_us) <- c("alpha", "beta", "gamma")
 
+# Save output 
 saveRDS(list(
-    model_us      = model_us,
-    lr_us         = lr_us,
-    model_china   = model_china,
-    lr_china      = lr_china,
-    model_germany = model_germany,
-    lr_germany    = lr_germany),
+    model_us       = model_us,
+    model_china    = model_china,
+    model_germany  = model_germany,
+    lr_us          = lr_us,
+    lr_china       = lr_china,
+    lr_germany     = lr_germany,
+    params_us      = params_us,
+    params_china   = params_china,
+    params_germany = params_germany),
     dir$lr_models
 )
